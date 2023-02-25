4742b47892e2fa949de601bea756ed3b66aa16fc
naderidev <mohammadrezanaderi84@gmail.com>
2023-02-25 16:32:47 +0330

    update project structure

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 import time
 from flet import *
-from src.pininput.input_types import RegularLinkedInputs
-from src.pininput.pininput import LinkedInputs, AcceptTypes , pprint
+from src.linkedinputs.linkedinputs import LinkedInputs, AcceptTypes, pprint, RegularLinkedInputs
 
 
 def main(page: Page):
     page.title = 'PIN INPUT'
     inputs = [
         TextField(
-        width=50,
-        text_size=20,
-        text_align=TextAlign.CENTER
+            width=100,
+            text_size=20,
+            text_align=TextAlign.CENTER
         ) for i in range(4)
     ]
 
     page.add(RegularLinkedInputs(
         page=page,
         inputs=inputs,
-        accept_type=AcceptTypes.ONLY_LETTERS,
+        accept_type=AcceptTypes.ONLY_NUMBER,
+        accept_length=4,
         on_error=lambda c: print('Error')
-        
+
     ))
 
 
--- src/linkedinputs/linkedinputs.py
+++ src/linkedinputs/linkedinputs.py
@@ -69,7 +69,7 @@ class LinkedInputs(UserControl):
             self.on_complete(self)
 
         print(self._errors)
-        if True in [ (True if isinstance(i , InputErrorTypes) else False) for i in self._errors] and self.on_error:
+        if True in [(True if isinstance(i, InputErrorTypes) else False) for i in self._errors] and self.on_error:
             self.on_error(self)
 
     def flash_errors(self):
@@ -132,3 +132,51 @@ class LinkedInputs(UserControl):
     @property
     def string_value(self):
         return ''.join(self.__vals)
+
+
+class RegularLinkedInputs(LinkedInputs):
+    def __init__(
+        self, page: Page,
+            inputs: Optional[TextField] = [],
+            accept_type: Optional[AcceptTypes] = AcceptTypes.ALL,
+            on_change: Optional[callable] = None,
+            on_error: Optional[callable] = None,
+            on_complete: Optional[callable] = None,
+            place: Optional[Row | Column] = Row(
+                vertical_alignment=CrossAxisAlignment.CENTER,
+                alignment=MainAxisAlignment.CENTER
+            ),
+            accept_length: int = 1,
+            one_by_one: bool = True
+
+    ):
+        super().__init__(
+            page=page,
+            inputs=inputs,
+            accept_type=accept_type,
+            on_change=on_change,
+            on_error=on_error,
+            on_complete=on_complete,
+            place=place
+        )
+        self.accept_length = accept_length
+        self.one_by_one = one_by_one
+
+    def _on_change(self, current_index: int, values: list = [], errors: list = []):
+        self.__validate_length(current_index, values[current_index])
+        if self.one_by_one \
+                and ((current_index + 1) < len(values)) \
+                and (len(values[current_index]) >= self.accept_length):
+            
+            self.inputs[current_index + 1].focus()
+
+    def __validate_length(self, current_index: int, value: str):
+        if not self.errors[current_index]:
+            if len(value) > self.accept_length:
+                self.errors[current_index] = InputErrorTypes.LENGTH_ERROR
+                self.inputs[current_index].value = value[0: self.accept_length]
+                self.inputs[current_index].update()
+                return False
+
+            self.errors[current_index] = None
+            return True
